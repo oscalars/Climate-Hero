ccc30aa48a6725d3793cabc5c947fbcf5185d442
racso99 <98709580+oscalars@users.noreply.github.com>
2022-09-24 21:44:57 +0200

    test push laptop

--- Code/app/src/main/java/com/example/climatehero/DatabaseConn.java
+++ Code/app/src/main/java/com/example/climatehero/DatabaseConn.java
@@ -14,6 +14,7 @@ public class DatabaseConn {
             String url = "jdbc:postgresql://ec2-34-242-84-130.eu-west-1.compute.amazonaws.com/d7eaj1ale21ggd";
             String username = "awqmxaffunzrcw";
             String password = "0a7bdd60f2d0269a6389d84c91f4f0fb0f0a6f132be701eb238f6629235076e9";
+            String url2;
             this.c = DriverManager.getConnection(url, username, password);
 
         } catch (Exception e) {
