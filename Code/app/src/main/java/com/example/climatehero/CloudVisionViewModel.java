119e0194c8b01562c441d7ae4bfde9240704e569
Johan Bergfalk <johanbergfalk@outlook.com>
2022-09-28 15:48:34 +0200

    Create view model for cloud vision communication

---
+++ Code/app/src/main/java/com/example/climatehero/CloudVisionViewModel.java
@@ -0,0 +1,27 @@
+package com.example.climatehero;
+
+import android.graphics.Bitmap;
+
+import androidx.lifecycle.ViewModel;
+
+public class CloudVisionViewModel extends ViewModel {
+
+    private Bitmap photo;
+    private String recognizedItem;
+
+    public Bitmap getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Bitmap photo) {
+        this.photo = photo;
+    }
+
+    public String getRecognizedItem() {
+        return recognizedItem;
+    }
+
+    public void setRecognizedItem(String recognizedItem) {
+        this.recognizedItem = recognizedItem;
+    }
+}
