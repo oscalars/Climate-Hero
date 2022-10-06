3d4d4f233bee61cbe55db1f1d516af345caeb837
Johan Bergfalk <johanbergfalk@outlook.com>
2022-10-06 16:13:45 +0200

    Ask the user for permission to use camera

--- Code/app/src/main/java/com/example/climatehero/View/FirstFragment.java
+++ Code/app/src/main/java/com/example/climatehero/View/FirstFragment.java
@@ -1,19 +1,27 @@
 package com.example.climatehero.View;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
@@ -45,8 +53,12 @@ public class FirstFragment extends Fragment {
         binding.openCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                cameraLauncher.launch(intent);
+                if (ContextCompat.checkSelfPermission(
+                        getContext().getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    launchCamera();
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                }
             }
         });
     }
@@ -67,6 +79,23 @@ public class FirstFragment extends Fragment {
                     }
                 }
             });
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                Log.d("Inside permission launcher", isGranted.toString());
+                if (isGranted) {
+                    launchCamera();
+                } else {
+                    Toast.makeText(getActivity(),
+                            "Climate Hero needs camera access to work properly" ,
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraLauncher.launch(intent);
+    }
 
     @Override
     public void onDestroyView() {
